7ada710fcb0d910fbc9e1711c993eab4abf5a6f5
marco mancini <marco.mancini@obspm.fr>
2017-03-15 22:00:11 +0100

    works fine

---
+++ README.md
@@ -0,0 +1,10 @@
+# MMMenuLib
+Is a simple library based which permits to create nested dropdown menus.
+
+MMMenuLib is based exclusively on JavaScript.
+
+CSS additional libraries permit to customize easily the menus created.
+
+## Installation
+In the file index.html there are three simple examples of menus.
+To run in a web server.
--- js/menu.js
+++ js/menu.js
@@ -3,122 +3,102 @@
 (function(exports){
 
 
-
-  /*  for all Menus */
-  var MMMenu = [];
-
-
-  /* Base name for Ids and Classes in html */
-  var MMMname = 'MMMenu',
-      container_base = MMMname + '_main_',
-      closingDiv_base = MMMname + '_div_',
-      title_base = MMMname + '_a_',
-      button_base = MMMname + '_caret_'
-  ;
-
-  var NameSet = function(name){
-    MMMname = name,
-    container_base = MMMname + '_main_',
-    closingDiv_base = MMMname + '_div_',
-    title_base = MMMname + '_a_',
-    button_base = MMMname + '_caret_'
-    ;
-  };
+  /* Default basename for Ids and Classes in html */
+  var MMMname = 'MMMenu' ;
 
 
-  /* Class for a single accordeon menu*/
-  var DropFuncClass = function (id,options) {
-    this.id = id;
 
-    //if(id > 9){throw 'MMMenu accept 10 submenus maximum'; }
+  var MMMenuClass = function(options){
 
-    var closingDiv_id = closingDiv_base + id,
-	title_id = title_base + id,
-	button_id =  button_base + id;
-
-
-    var _memId = closingDiv_id+'_status',
-	_status = localStorage.getItem(_memId)
-    ;
-
-    var _ele, _caret;
+    /*  for all Menus */
+    var MMMenu = [];
 
     options = options || {};
-    var _cascade = options['cascade'] || true ,
-	_upSymbol = options['upSymbol'] || '-' ,
-	_downSymbol = options['downSymbol'] || '+' ;
-
-
-
-    _status = (_status==='open') ? 'open' :'closed';
-
-    var that = this;
-
-    this.init = function(){
-      _ele = document.getElementById(closingDiv_id);
-      _ele.style.display = 'none';
-      document.getElementById(title_id).onclick = that.flipmenu;
-      _caret = document.getElementById(button_id);
-      _caret.innerHTML = _downSymbol;
-      if(_status==='open'){that.open(); }
-
-      return this;
-    };
-
-    this.close = function (waittime) {
-      waittime = waittime | 0;
-      setTimeout(
-	function(){
-	  _caret.innerHTML = _downSymbol;
-	  _ele.style.display = 'none';
-	}, waittime);
-      if(_cascade){
-	console.log(_cascade,MMMname,closingDiv_id)
-      	var childs = document.getElementById(closingDiv_id).getElementsByClassName(MMMname);
-      	for(var ii=0; ii<childs.length; ii++){
-      	  var locid = childs[ii].id;
-      	  MMMenu[locid[locid.length-1]].close();
-      	}
-      }
-      _status = 'closed';
-    };
-
-    this.open =function (waittime) {
-      waittime = waittime | 0;
-      setTimeout(
-	function(){
-	  _caret.innerHTML = _upSymbol;
-	  _ele.style.display = 'block';
-	},waittime);
-      _status = 'open';
-    };
-
-
-    this.flipmenu = function(){
-
-      (_status==='closed') ? that.open() : that.close();
-
-      localStorage.setItem(_memId, _status);
-
-    };
-
-    return this;
-  };
-
-
-
-  var CreateMenus = function(options){
-    for(var ii in options){
-      switch(ii){
-      case 'name':
-	NameSet(options[ii]); break;
-      }
-    }
+    MMMname = options['name']|| MMMname;
 
     var menus = document.getElementsByClassName(MMMname);
 
+
+    /* Class for a single accordeon menu*/
+    var DropFuncClass = function (id,options) {
+      this.id = id;
+
+      //if(id > 9){throw 'MMMenu accept 10 submenus maximum'; }
+
+      var _ele, _caret;
+
+      options = options || {};
+      var _cascade = options['cascade'] || true ,
+	  _upSymbol = options['upSymbol'] || '-' ,
+	  _downSymbol = options['downSymbol'] || '+',
+	  _name = options['name']|| MMMname
+      ;
+
+
+      var _closingDiv_id = _name +'_div_' + id,
+	  _title_id = _name +'_a_' + id,
+	  button_id = _name +'_caret_' + id;
+
+      var _memId = _closingDiv_id+'_status',
+	  _status = localStorage.getItem(_memId)
+      ;
+
+      _status = (_status==='open') ? 'open' :'closed';
+
+      _ele = document.getElementById(_closingDiv_id);
+
+      var that = this;
+
+      this.init = function(){
+	_ele.style.display = 'none';
+	document.getElementById(_title_id).onclick = that.flipmenu;
+	_caret = document.getElementById(button_id);
+	_caret.innerHTML = _downSymbol;
+
+	if(_status==='open'){that.open(); }
+
+	return this;
+      };
+
+      this.close = function (waittime) {
+	waittime = waittime | 0;
+	setTimeout(
+	  function(){
+	    _caret.innerHTML = _downSymbol;
+	    _ele.style.display = 'none';
+
+	  }, waittime);
+	if(_cascade){
+      	  var childs = _ele.getElementsByClassName(_name);
+      	  for(var ii=0; ii<childs.length; ii++){
+      	    var locid = childs[ii].id;
+      	    MMMenu[locid[locid.length-1]].close();
+      	  }
+	}
+	_status = 'closed';
+      };
+
+      this.open =function (waittime) {
+	waittime = waittime | 0;
+	setTimeout(
+	  function(){
+	    _caret.innerHTML = _upSymbol;
+	    _ele.style.display = 'block';
+	  },waittime);
+	_status = 'open';
+      };
+
+      this.flipmenu = function(){
+	(_status==='closed') ? that.open() : that.close();
+	localStorage.setItem(_memId, _status);
+      };
+      return this;
+    };
+
+
+
     for(var imenu=0;imenu<menus.length;imenu++){
-      menus[imenu].id = container_base + imenu;
+      menus[imenu].id = MMMname + '_main_' + imenu;
 
       var menu_parts = menus[imenu].children;
       for (var i = 0; i < menu_parts.length; i++) {
@@ -126,15 +106,15 @@
 
 	switch (menu_parts[i].tagName) {
 	case 'A':
-	  locpart.id = title_base + imenu;
+	  locpart.id = MMMname + '_a_' + imenu;
 	  locpart.className = locpart.className + ' '+MMMname+'Title';
 	  var idc = locpart.getElementsByTagName('I').length - 1;
 	  var caret = locpart.getElementsByTagName('I')[idc];
-	  caret.id = button_base + imenu;
+	  caret.id = MMMname + '_caret_' + imenu;
 	  caret.className = caret.className + ' '+MMMname+'Caret';
 	  break;
 	case 'DIV':
-	  locpart.id = closingDiv_base + imenu;
+	  locpart.id = MMMname + '_div_' + imenu;
 	  locpart.className = locpart.className + ' '+MMMname+'Div';
 	  break;
 	default:
@@ -142,17 +122,16 @@
 	}
       }
       MMMenu.push(new DropFuncClass(imenu,options).init());
-
     }
 
+    return this;
+
   };
-
-
   /* Exported fuctions */
-  exports.CreateMenus = CreateMenus;
+  exports.MMMenuClass = MMMenuClass;
 
   /* To export for debug */
   // exports.DropFuncClass = DropFuncClass;
   // exports.MMMenu = MMMenu;
 
-  })(this.MMMenuLib = {});
+})(this.MMMenuLib = {});
